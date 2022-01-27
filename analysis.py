d2c580c6183ed9f9f474427a50fd87c2ea8348c1
Frank Bryce <jonnyjack7@gmail.com>
2022-01-27 14:34:28 -0500

    Add a resp: set(word_tuple) setup to analysis.
    This will drastically speed up the algorithm if it fit in memory. The
    test verifies that it in fact does so this is good. Let's make it
    happen in play.py.

--- analysis.py
+++ analysis.py
@@ -50,6 +50,45 @@ def main():
     print([(w, score) for w, score in sorted(scores.items(), key=lambda item: item[1], reverse=True)][:10])
 
 
+def getResp(guess, master):
+    # 0 is miss, 1 is wrong spot, 2 is right spot
+    resp = [0,0,0,0,0]
+    mastercpy = list(master)
+    for i in range(5):
+        if guess[i] == master[i]:
+            resp[i] = 2
+            mastercpy.pop(i-(5-len(mastercpy)))
+    for c in mastercpy:
+        c_in_guess = -1
+        for i in range(5):
+            if resp[i] != 0:
+                continue
+            if guess[i] == c:
+                c_in_guess = i
+                break
+        if c_in_guess >= 0:
+            resp[c_in_guess] = 1
+    return tuple(resp)
+
+def main2():
+    words = []
+    with open("dict.txt", "r") as f:
+        for w in f.readlines():
+            w = w.strip()  # trailing \n for some reason
+            words.append(w)
+
+    rdict = dict()
+    for w1 in tqdm(words):
+        for w2 in words:
+            r = getResp(w1, w2)
+            if r not in rdict:
+                rdict[r] = set()
+            rdict[r].add((w1,w2))
+
+    for r, t in sorted(rdict.items(), key=lambda item: len(item[1])):
+        print(r, len(t), sep=':')
+    print(len(rdict))
 
 if __name__ == "__main__":
     main()
+    main2()
